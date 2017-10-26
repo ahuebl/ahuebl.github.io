49a0ac997e7efe837f793cf9f6df85d1d23a61ba
Allyson <allysonhuebl@cocc.edu>
2017-10-26 08:30:08 -0700

    building on to battleship

--- scripts/battleship.js
+++ scripts/battleship.js
@@ -1,33 +1,47 @@
-let randomLoc = Math.floor(Math.random() * 5);
-let location1 = randomLoc;
-let location2 = location1 + 1;
-let location3 = location2 + 1;
-let guess;
-let hits = 0;
-let guesses = 0;
-let isSunk = false;
+var view = {
+    displayMessage: function(msg) {
+        var messageArea = document.getElementById("messageArea");
+        messageArea.innerHTML = msg;
+    },
+    displayHit: function(location) {
+        var cell = document.getElementById(location);
+        cell.setAttribute("class", "hit");
+    },
+    displayMiss: function(location) {
+        var cell = document.getElementById(location);
+        cell.setAttribute("class", "miss"); 
+    }
+};
 
-while (isSunk === false) {
-    guess = prompt("Ready, aim, fire! (enter a number from 0-6) :");
-if (guess < 0 || guess > 6) {
-    alert("Please enter a valid cell number!");
-}
-else {
-    guesses = guesses + 1;
-}
-if (guess == location1 || guess == location2 || guess == location3) {
-    alert("HIT!");
-    hits = hits + 1;
-}
-else {
-    alert("MISS!");
-}
-if (hits === 3) {
-    isSunk = true;
-    alert("You sank my battleship!");
-}
-}
+view.displayMiss("00");
+view.displayHit("34");
+view.displayMiss("55");
+view.displayHit("12");
+view.displayMiss("25");
+view.displayHit("26");
 
-let stats = "You took " + guesses + " guesses to sink the battleship, " +
-            "which means your shooting accuracy was " + (3/guesses)*100 + "%";
-alert(stats);
+view.displayMessage("Tap tap, is this thing on?");
+
+var model = {
+    boardSize: 7,
+    numShips: 3,
+    shipLength: 3,
+    shipsSunk: 0,
+
+    ships: [ { locations: ["06", "16", "26"], hits: ["", "", ""] },
+             { locations: ["24", "34", "44"], hits: ["", "", ""] },
+             { locations: ["10", "11", "12"], hits: ["", "", ""] }, ],
+
+    fire: function(guess) {
+        for (var i = 0; i < this.numShips; i += 1) {
+            var ship = this.ships[i];
+            var index = ship.locations.indexOf(guess);
+            if (index >= 0) {
+                ship.hits[index] = "hit";
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
