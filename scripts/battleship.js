4c5a9d5b5b9a7a19c4d4f82d070f3d59f3dcbd55
Allyson <32317454+ahuebl@users.noreply.github.com>
2017-11-17 07:14:12 -0800

    toUpperCase & got rid of alerts

--- scripts/battleship.js
+++ scripts/battleship.js
@@ -160,18 +160,18 @@ function parseGuess(guess) {
     var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
 
     if (guess === null || guess.length !== 2) {
-        alert("Oops, please enter a letter and a number on the board.");
+        view.displayMessage("Oops, please enter a letter and a number on the board.");
     }
     else {
-        var firstChar = guess.charAt(0);
+        var firstChar = guess.charAt(0).toUpperCase();
         var row = alphabet.indexOf(firstChar);
         var column = guess.charAt(1);
 
         if (isNaN(row) || isNaN(column)) {
-            alert("Oops, that isn't on the board.");
+            view.displayMessage("Oops, that isn't on the board.");
         }
         else if (row < 0 || row >= model.boardsize || column < 0 || column >= model.boardSize) {
-            alert("Oops, that's off the board!");
+            view.displayMessage("Oops, that's off the board!");
         }
         else {
             return row + column;
